6fa25bc1834703fad3f718f4318f94c6b3b4cc33
Adolfo Herrera <adolfoherrera1417@gmail.com>
2020-09-16 15:57:20 -0500

    Fully functional A* Algo

--- src/Algorithms/Astar.js
+++ src/Algorithms/Astar.js
@@ -1,4 +1,4 @@
-// const { Heap } = require('heap-js');
+const { Heap } = require('heap-js');
 //https://github.com/ignlg/heap-js
 
 // Node class to calculate its own values
@@ -7,16 +7,18 @@
 //hCost = distance from the target node
 //fCost = gCost + hCost
 
-// const PriorityQueue = require('./pq.js');
+let neighbors = [[0,1],[0,-1],[1,0],[-1,0]];
 
 class sNode {
     bObstacle = false;
-    visted = false;
+    opened = false;
+    closed = false;
     gCost = Infinity;
     fCost = Infinity;
     parent;
     x;
     y;
+    isWall = false;
 
     // constructor
     constructor(x, y) {
@@ -25,144 +27,97 @@ class sNode {
     }
 }
 
-// const queue = new PriorityQueue();
-
-
 const createNodeMatrix = (matrix) => {
     let nodeMatrix = [];
     for(let i = 0; i < matrix.length; i++) {
         let nodeMatrixRow = [];
         for (let j = 0; j < matrix[0].length; j++) {
-            nodeMatrixRow.push(new sNode(i,j));
+            let temp = new sNode(i,j)
+            if (matrix[i][j] === 'W') {
+                temp.isWall = true;
+            }
+            nodeMatrixRow.push(temp);
+            
         }
         nodeMatrix.push(nodeMatrixRow);
     }
     return nodeMatrix;
 }
 
-const solve = (matrix, endNode) => {
-    // Create a min Queue based on nodes fcost value
-    console.log("++++++++++++++++++++++")
+const solve = (graph, startNode, endNode) => {
     const customPriorityComparator = (a, b) => a.fCost - b.fCost;
-    let openList = new Heap(customPriorityComparator);
-    let openList = new PriorityQueue((a, b) => a.fCost <= b.fCost);
-    let closedList = new Set();
     let visitedPathInOrder = [];
-    let startNode = matrix[0][0];
+    let openList = new Heap(customPriorityComparator);
+    let SQRT2 = Math.SQRT2;
+    let m = graph.length;
+    let n = graph[0].length;
+    //Grab actual node object from graph
+    startNode = graph[startNode.x][startNode.y];
+    endNode = graph[endNode.x][endNode.y];
+
     startNode.gCost = 0;
-    startNode.hCost = manhattanDistance(startNode,endNode);
-    startNode.fCost = startNode.gCost + startNode.hCost;
-    matrix[0][0] = startNode;
+    startNode.fCost = 0;
+
     openList.push(startNode);
+    startNode.opened = true;
     
-    let count = 0;
-
     while(!openList.isEmpty()) {
-        let currentNode = openList.peek();
-        console.log(currentNode);
+        let node = openList.peek();
         openList.pop();
-        closedList.add(currentNode);
+        visitedPathInOrder.push(node);
+        node.closed = true;
+        graph[node.x][node.y] = node;
+        if (node === endNode) {
+            let path = reconstructPath(endNode);
+            return {path,visitedPathInOrder};
+        }
 
-        if (currentNode.x === endNode.x && currentNode.y === endNode.y) {
-            break;
+        for (let i = 0; i < neighbors.length; i++) {
+            let r = node.x+neighbors[i][0];
+            let c = node.y+neighbors[i][1];
+            if (r < 0 || r >= m || c < 0 || c >= n) {
+                continue;
+            }
+
+            let neighbor = graph[r][c];
+            const tempNeighbor = graph[r][c];
+            if (neighbor.closed || neighbor.isWall) {
+                continue;
+            }
+
+            let x = neighbor.x;
+            let y = neighbor.y;
+
+            let possibleGCost = node.gCost + ((x - node.x === 0 || y - node.y === 0) ? 1 : SQRT2);
+
+            if (!neighbor.opened || possibleGCost < neighbor.gCost) {
+                neighbor.gCost = possibleGCost;
+                neighbor.fCost = neighbor.gCost + 10 * manhattanDistance(neighbor,endNode);
+                neighbor.parent = node;
+
+                if (!neighbor.opened) {
+                    openList.push(neighbor);
+                    neighbor.opened = true;
+                    graph[neighbor.x][neighbor.y] = neighbor;
+                } else {
+                    openList.remove(tempNeighbor);
+                    openList.push(neighbor)
+                }
+            }
+
         }
         
-        visitedPathInOrder.push(currentNode);
-        currentNode.visted = true;
-        //Grab LEFT node
-        let neighbor;
-        if (currentNode.y - 1 >= 0 && !closedList.has(matrix[currentNode.x][currentNode.y-1])) {
-            neighbor = matrix[currentNode.x][currentNode.y-1];
-            let tentativeGCost = currentNode.gCost + manhattanDistance(neighbor,currentNode);
-            
-            if (tentativeGCost < neighbor.gCost) {
-                neighbor.parent = currentNode;
-                neighbor.gCost = tentativeGCost;
-                neighbor.fCost = neighbor.gCost + manhattanDistance(neighbor,endNode);
-            }
-            matrix[neighbor.x][neighbor.y] = neighbor;
-            if (!openList.contains(neighbor)) {
-                openList.push(neighbor);
-            }
-        }
-
-        //Grab RIGHT node
-        if (currentNode.y + 1 < matrix.length && !closedList.has(matrix[currentNode.x][currentNode.y+1])) {
-            neighbor = matrix[currentNode.x][currentNode.y+1];
-
-            let tentativeGCost = currentNode.gCost + manhattanDistance(neighbor,currentNode);
-            console.log("Calcualting Bottom Node...")
-            console.log("tentativGCost: " + tentativeGCost);
-            if (tentativeGCost < neighbor.gCost) {
-                neighbor.parent = currentNode;
-                neighbor.gCost = tentativeGCost;
-                neighbor.fCost = neighbor.gCost + manhattanDistance(neighbor,endNode);
-                console.log("Manhattan Distance: " + manhattanDistance(neighbor,endNode))
-                console.log("fCost: " + neighbor.fCost);
-            }
-            matrix[neighbor.x][neighbor.y] = neighbor;
-            if (!openList.contains(neighbor)) {
-                openList.push(neighbor);
-            }
-        }
-        
-        //Grab TOP node
-        if (currentNode.x - 1 >= 0 && !closedList.has(matrix[currentNode.x-1][currentNode.y])) {
-            neighbor = matrix[currentNode.x - 1][currentNode.y];
-        
-            let tentativeGCost = currentNode.gCost + manhattanDistance(neighbor,currentNode);
-            
-            if (tentativeGCost < neighbor.gCost) {
-                neighbor.parent = currentNode;
-                neighbor.gCost = tentativeGCost;
-                neighbor.fCost = neighbor.gCost + manhattanDistance(neighbor,endNode);
-            }
-            matrix[neighbor.x][neighbor.y] = neighbor;
-            if (!openList.contains(neighbor)) {
-                openList.push(neighbor);
-            }
-        }
-
-        //Grab BOTTOM node
-        if (currentNode.x + 1 < matrix[0].length && !closedList.has(matrix[currentNode.x+1][currentNode.y])) {
-            neighbor = matrix[currentNode.x + 1][currentNode.y];
-            console.log('--------------------');
-            let tentativeGCost = currentNode.gCost + manhattanDistance(neighbor,currentNode);
-
-            console.log("Calcualting Right Node...")
-            console.log("tentativGCost: " + tentativeGCost);
-            if (tentativeGCost < neighbor.gCost) {
-                
-                neighbor.parent = currentNode;
-                neighbor.gCost = tentativeGCost;
-                neighbor.fCost = neighbor.gCost + manhattanDistance(neighbor,endNode);
-
-                console.log("Manhattan Distance: " + manhattanDistance(neighbor,endNode))
-                console.log("fCost: " + neighbor.fCost);
-            }
-            matrix[neighbor.x][neighbor.y] = neighbor;
-            if (!openList.contains(neighbor)) {
-                openList.push(neighbor);
-            }
-        }
-        matrix[currentNode.x][currentNode.y] = currentNode;
-        count = count + 1;
-        console.log("++++++++++++++++++++++")
     }
-    let path = matrix;
-    return {path,closedList,visitedPathInOrder};
+    return [];
 }
 
 const manhattanDistance = (current_cell, goal) => {
-    let dstX = Math.abs(current_cell.x  - goal.x);
-    let dstY = Math.abs(current_cell.y - goal.y);
-    if (dstX > dstY) {
-        return 14*dstY + 10* (dstX-dstY);
-    }
-    return 14*dstX + 10 * (dstY-dstX);
+    let dx = Math.abs(current_cell.x  - goal.x);
+    let dy = Math.abs(current_cell.y - goal.y);
+    return dx + dy;
 }
 
-const reconstructPath = (matrix,endNode) => {
+const reconstructPath = (endNode) => {
     let path = []
     while(endNode.parent != undefined) {
         path.push({x: endNode.x, y: endNode.y});
@@ -173,27 +128,10 @@ const reconstructPath = (matrix,endNode) => {
     return path;
 }
 
-const reconstructVisitedPath = (closedList) => {
-    let visitedPathInOrder = []
-    for (let i = 0; i < closedList.length; i++) {
-        visitedPathInOrder.push({x: closedList[i].x,y: closedList[i].y});
-    }
-    return visitedPathInOrder;
-}
-export const  AstarShortestPath = (matrix, endNode) => {
-    let nodeMatrix = createNodeMatrix(matrix);
-    let {path,closedList,visitedPathInOrder} = solve(nodeMatrix,endNode);
-    let legitPath = reconstructPath(path,path[endNode.x][endNode.y]);
-    visitedPathInOrder = reconstructVisitedPath(visitedPathInOrder)
-    path = legitPath
+export const AstarShortestPath = (matrix, startNode, endNode) => {
+    let graph = createNodeMatrix(matrix);
+    let {path,visitedPathInOrder} = solve(graph,startNode,endNode);
+    console.log(visitedPathInOrder.length)
     return {path,visitedPathInOrder};
 }
 
-let tempmatrix = [];
-for (let i = 0; i < 10;i++) {
-    tempmatrix.push(new Array(10).fill("E"))
-}
-
-// let {path,visitedPathInOrder} = AstarShortestPath(tempmatrix,{x:1, y:7});
-// console.log(path)
-// console.log(visitedPathInOrder.length);
