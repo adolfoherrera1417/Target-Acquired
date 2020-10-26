362a9e3ba4a05ba8b79d730ebea8ce22a77bf675
Adolfo Herrera <adolfoherrera1417@gmail.com>
2020-10-26 18:28:56 -0500

    Updates

---
+++ precache-manifest.656abe44ef55210ea4191486be3d5d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1061fa0eb3a8848851444034620340e0",
+    "url": "/Target-Acquired/index.html"
+  },
+  {
+    "revision": "f256e2f415c1b51b7539",
+    "url": "/Target-Acquired/static/css/main.7aaeb9f3.chunk.css"
+  },
+  {
+    "revision": "7fc4784e23c435642f22",
+    "url": "/Target-Acquired/static/js/2.829ebc40.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Target-Acquired/static/js/2.829ebc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f256e2f415c1b51b7539",
+    "url": "/Target-Acquired/static/js/main.468b647e.chunk.js"
+  },
+  {
+    "revision": "dd00d7dad66b03bff0c8",
+    "url": "/Target-Acquired/static/js/runtime-main.8a69beed.js"
+  }
+]);
